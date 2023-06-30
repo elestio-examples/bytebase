d3fd00a2665d9eca3fb9691c41b96505bb0d2649
Muthiah131998 <110098347+Muthiah131998@users.noreply.github.com>
2023-06-30 19:28:33 +0530

    Update build.sh

--- tests/build.sh
+++ tests/build.sh
@@ -1,1 +1,6 @@
-
+docker build -f ./scripts/Dockerfile \
+    --build-arg VERSION=${VERSION} \
+    --build-arg GIT_COMMIT="$(git rev-parse HEAD)"\
+    --build-arg BUILD_TIME="$(date -u +"%Y-%m-%dT%H:%M:%SZ")"  \
+    --build-arg BUILD_USER="$(id -u -n)" \
+    -t elestio4test/bytebase .
