7cda75c202371207af067f91839143e5d398e88f
Muthiah131998 <110098347+Muthiah131998@users.noreply.github.com>
2023-07-12 21:30:09 +0530

    Update build.sh

--- tests/build.sh
+++ tests/build.sh
@@ -1,6 +1,7 @@
-docker buildx build -f ./scripts/Dockerfile \
+cp ./scripts/Dockerfile ./
+docker buildx build . \
     --build-arg VERSION=${VERSION} \
     --build-arg GIT_COMMIT="$(git rev-parse HEAD)"\
     --build-arg BUILD_TIME="$(date -u +"%Y-%m-%dT%H:%M:%SZ")"  \
     --build-arg BUILD_USER="$(id -u -n)" \
-    --output type=docker,name=elestio4test/bytebase:latest .
+    --output type=docker,name=elestio4test/bytebase:latest | docker laod
