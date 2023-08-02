7ecc4ed12c1526c805deeb4fbf7ab039540858d0
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-08-02 18:13:04 +0300

    fix

--- tests/build.sh
+++ tests/build.sh
@@ -1,7 +1,5 @@
-cp ./scripts/Dockerfile ./
-docker buildx build . \
-    --build-arg VERSION=${VERSION} \
-    --build-arg GIT_COMMIT="$(git rev-parse HEAD)"\
-    --build-arg BUILD_TIME="$(date -u +"%Y-%m-%dT%H:%M:%SZ")"  \
-    --build-arg BUILD_USER="$(id -u -n)" \
-    --output type=docker,name=elestio4test/bytebase:latest | docker load
+cp ./scripts/* ./
+
+VERSION=$(cat ./VERSION)
+
+docker buildx build . --output type=docker,name=elestio4test/bytebase:latest | docker load
