5e8584e8bce2cfba5e6fbfe0bff80b69ddf37696
Prem Chavhan <104148610+premchavhan99@users.noreply.github.com>
2023-08-17 17:03:12 +0530

    Update build.sh

--- tests/build.sh
+++ tests/build.sh
@@ -7,13 +7,9 @@ GIT_COMMIT="$(git rev-parse HEAD)"
 BUILD_TIME="$(date -u +"%Y-%m-%dT%H:%M:%SZ")"  
 BUILD_USER="$(id -u -n)"
 
-sed -i 's~ARG VERSION="development"~ARG VERSION="production"~g' ./Dockerfile.bb
-sed -i 's~ARG GIT_COMMIT="unknown"~ARG GIT_COMMIT="'${GIT_COMMIT}'"~g' ./Dockerfile.bb
-sed -i 's~ARG BUILD_TIME="unknown"~ARG BUILD_TIME="'${BUILD_TIME}'"~g' ./Dockerfile.bb
-sed -i 's~ARG BUILD_USER="unknown"~ARG BUILD_USER="'${BUILD_USER}'"~g' ./Dockerfile.bb
-
-rm ./Dockerfile
-
-cp ./Dockerfile.bb ./Dockerfile
+sed -i 's~ARG VERSION="development"~ARG VERSION="production"~g' ./Dockerfile
+sed -i 's~ARG GIT_COMMIT="unknown"~ARG GIT_COMMIT="'${GIT_COMMIT}'"~g' ./Dockerfile
+sed -i 's~ARG BUILD_TIME="unknown"~ARG BUILD_TIME="'${BUILD_TIME}'"~g' ./Dockerfile
+sed -i 's~ARG BUILD_USER="unknown"~ARG BUILD_USER="'${BUILD_USER}'"~g' ./Dockerfile
 
 docker buildx build . --output type=docker,name=elestio4test/bytebase:latest | docker load
